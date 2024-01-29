4d9be03829374deff0059e781fc67f148c1fe495
Alwan Wirawan <alwan.wirawan@djembatandua.com>
2024-01-29 13:09:05 +0700

    create personal information component

---
+++ src/form/personal-information/PersonalInformation.js
@@ -0,0 +1,35 @@
+import { Form, Input, Button } from "antd";
+
+const PersonalInformation = () => {
+  const [form] = Form.useForm();
+
+  return (
+    <Form form={form}>
+      <Form.Item
+        name="fullName"
+        label="Full Name"
+        rules={[{ type: "text", required: true }]}
+      >
+        <Input />
+      </Form.Item>
+
+      <Form.Item
+        name="email"
+        label="Email"
+        rules={[{ type: "email", required: true }]}
+      >
+        <Input />
+      </Form.Item>
+
+      <Form.Item
+        name="birthDate"
+        label="Birth Date"
+        rules={[{ type: "date", required: true }]}
+      >
+        <Input />
+      </Form.Item>
+    </Form>
+  );
+};
+
+export default PersonalInformation;
