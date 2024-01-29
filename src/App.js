ea64deb8411a7639e3b876f115eb87c0ce52bf36
Alwan Wirawan <alwan.wirawan@djembatandua.com>
2024-01-29 12:58:41 +0700

    define layout

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
+import { Flex, Layout } from "antd";
+import { Content, Footer, Header } from "antd/es/layout/layout";
+import "../src/css/main.css";
+
 function App() {
   return (
-    <>
-    
-    </>
+    <Flex>
+      <Layout>
+        <Header>Header</Header>
+        <Content>Content</Content>
+        <Footer>Footer</Footer>
+      </Layout>
+    </Flex>
   );
 }
 
