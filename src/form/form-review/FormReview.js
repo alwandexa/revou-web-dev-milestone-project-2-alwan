2914c3c73a47ad67f8878bca1df1b73519dd5edb
Alwan Wirawan <alwan.wirawan@djembatandua.com>
2024-01-31 11:12:17 +0700

    create form review component

---
+++ src/form/form-review/FormReview.js
@@ -0,0 +1,26 @@
+import { Card, Form } from "antd";
+import PersonalInformation from "../personal-information/PersonalInformation";
+import AddressInformation from "../address-information/AddressInformation";
+import AccountInformation from "../account-information/AccountInformation";
+import Title from "antd/lib/typography/Title";
+
+const FormReview = ({ form }) => {
+  return (
+    <Form layout="vertical" disabled="true">
+      <Title level={3}>Personal Information</Title>
+      <Card>
+        <PersonalInformation form={form} />
+      </Card>
+      <Title level={3}>Address Information</Title>
+      <Card>
+        <AddressInformation form={form} />
+      </Card>
+      <Title level={3}>Account Information</Title>
+      <Card>
+        <AccountInformation form={form} />
+      </Card>
+    </Form>
+  );
+};
+
+export default FormReview;
