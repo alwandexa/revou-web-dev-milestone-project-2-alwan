0eb1185c0be8f0c37e7547e03696eeb17763fa90
Alwan Wirawan <alwan.wirawan@djembatandua.com>
2024-01-31 11:02:32 +0700

    change layout

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
-import { Flex, Layout } from "antd";
-import { Content, Footer, Header } from "antd/es/layout/layout";
+import { Layout } from "antd";
+import { Content } from "antd/es/layout/layout";
 import "../src/css/main.css";
+import RegistrationForm from "./form/registration-form/RegistrationForm";
+import SideInfo from "./side-info/SideInfo";
 
 function App() {
   return (
-    <Flex>
-      <Layout>
-        <Header>Header</Header>
-        <Content>Content</Content>
-        <Footer>Footer</Footer>
-      </Layout>
-    </Flex>
+    <Layout>
+      <Content>
+        <div className="main-content">
+          <SideInfo />
+          <RegistrationForm />
+        </div>
+      </Content>
+    </Layout>
   );
 }
 
