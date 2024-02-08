08e3c3b43d62edbdcd89293d51558d4f5920782a
alwandexa <alwan.wirawan@djembatandua.com>
2024-02-09 05:33:24 +0700

    split css for header

--- src/components/dexa-header/DexaHeader.tsx
+++ src/components/dexa-header/DexaHeader.tsx
@@ -1,18 +1,19 @@
 import { Select } from "antd";
 
 import i18n from "../../internalization/i18n";
+import "../../css/header.css";
 
 const DexaHeader = ({ t }: any) => {
     const changeLanguage = (language: string) => {
         i18n.changeLanguage(language);
     };
-    
+
     return (
         <>
             <img src={process.env.PUBLIC_URL + "logo-white.png"} alt="Company Logo" />
             <input type="search" placeholder={t("search-placeholder")} />
             <Select
-                defaultValue="en"
+                defaultValue={i18n.language}
                 onChange={(language) => changeLanguage(language)}
                 options={[
                     { value: 'en', label: 'EN' },
