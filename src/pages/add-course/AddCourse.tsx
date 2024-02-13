9380d963761b61f5d9f6e02610eefc46c9af93e9
alwandexa <alwan.wirawan@djembatandua.com>
2024-02-13 09:48:01 +0700

    create function to add course

--- src/pages/add-course/AddCourse.tsx
+++ src/pages/add-course/AddCourse.tsx
@@ -2,21 +2,30 @@ import { Button, Card, Form, Input, Layout } from "antd";
 import DexaHeader from "../../components/dexa-header/DexaHeader";
 import { Content, Footer, Header } from "antd/es/layout/layout";
 import { useTranslation } from "react-i18next";
+import { useNavigate } from "react-router-dom";
 
 import "../../css/add-item.css";
 
 const AddCourse = () => {
     const [form] = Form.useForm();
     const { t } = useTranslation();
+    const navigate = useNavigate();
+
+    const addCourse = () => {
+        let courses = JSON.parse(localStorage.getItem("courses") || "[]");
+
+        courses.push({
+            course_name: form.getFieldValue("Course"),
+            instructor: form.getFieldValue("Instructor"),
+            enroll_date: new Date().toLocaleDateString(),
+        });
+
+        localStorage.setItem("courses", JSON.stringify(courses));
+    }
 
     const handleSubmit = () => {
-        localStorage.setItem("course", form.getFieldValue("Course"));
-        localStorage.setItem("instructor", form.getFieldValue("Instructor"));
-        localStorage.setItem("enroll-date", new Date().toLocaleDateString());
-
-        console.log(localStorage.getItem("course"));
-        console.log(localStorage.getItem("instructor"));
-        console.log(localStorage.getItem("enroll-date"))
+        addCourse();
+        navigate("/");
     };
 
     return (
