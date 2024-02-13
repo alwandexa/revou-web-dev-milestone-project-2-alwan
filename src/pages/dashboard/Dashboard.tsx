2e0881cb4c722966a4b30bb92eeb43e157d532e2
alwandexa <alwan.wirawan@djembatandua.com>
2024-02-13 14:44:19 +0700

    refactor dashboard

--- src/pages/dashboard/Dashboard.tsx
+++ src/pages/dashboard/Dashboard.tsx
@@ -1,115 +1,38 @@
-import { useEffect, useState } from "react";
-import { useNavigate } from "react-router-dom";
-import { useRecoilValue } from "recoil";
 import { useTranslation } from "react-i18next";
-import { Button, Card, Col, Layout, Row, Timeline } from "antd"
+import { Col, Layout, Row } from "antd"
 import { Content, Footer, Header } from "antd/es/layout/layout";
-import Paragraph from "antd/es/typography/Paragraph";
-import Title from "antd/es/typography/Title";
-import { MenuFoldOutlined } from "@ant-design/icons";
 
-import { registeredData } from "../../recoil/atom/RegisteredData";
 import DexaHeader from "../../components/dexa-header/DexaHeader";
+import LearningHistory from "../../components/learning-history/LearningHistory";
+import CoursesTable from "../../components/courses-table/CoursesTable";
+import UserData from "../../components/user-data/UserData";
 
 const Dashboard = () => {
-    const user = useRecoilValue(registeredData);
-    const [reverse, setReverse] = useState(false);
+    const user = JSON.parse(localStorage.getItem("users") as string)[localStorage.getItem("userIndex") as string];
 
     const { t } = useTranslation();
-
-    const navigate = useNavigate();
-
-    useEffect(() => {
-        if (user["Name"] === "") {
-            navigate("/registration");
-        }
-    });
-
-    const timelineList = [
-        {
-            title: "Intro to React",
-            time: "09 JUN 7:20 PM",
-            color: "green",
-        },
-        {
-            title: "Advance CSS",
-            time: "08 JUN 12:20 PM",
-            color: "green",
-        },
-        {
-            title: "Advance HTML",
-            time: "04 JUN 3:10 PM",
-        },
-        {
-            title: "CSS Fundamental",
-            time: "02 JUN 2:45 PM",
-        },
-        {
-            title: "HTML Fundamental",
-            time: "18 MAY 1:30 PM",
-        },
-        {
-            title: "Intro to Web Development",
-            time: "14 MAY 3:30 PM",
-            color: "gray",
-        },
-    ];
-
-    const date = new Date(user.Birthdate);
 
     return (
         <Layout>
             <Header>
-                <DexaHeader t={t} />
+                <DexaHeader />
             </Header>
             <Content>
                 <div className="dashboard-content">
                     <h1>{t("greeting")}, {user.Name.toLocaleUpperCase()}!</h1>
-
                     <Row gutter={[24, 24]}>
-                        <Col xs={24} sm={24} md={12} lg={12} xl={16} className="">
-                            <Card>
-                                <Title level={2}>{t("your-data")}</Title>
-                                <Paragraph>
-                                    <Title level={4}>{t("name")}</Title>
-                                    {user.Name}
-                                    <Title level={4}>{t("address")}</Title>
-                                    {user.City}
-                                    <Title level={4}>{t("birthdate")}</Title>
-                                    {(date.getMonth() + 1) + '-' + date.getDate() + '-' + date.getFullYear()}
-                                    <Title level={4}>{t("birthplace")}</Title>
-                                    {user.Birthplace}
-                                </Paragraph>
-                            </Card>
+                        <Col xs={24} sm={24} md={12} lg={12} xl={16} >
+                            <Row gutter={[24, 24]}>
+                                <Col xs={24} sm={24} md={24} lg={24} xl={24} >
+                                    <UserData />
+                                </Col>
+                                <Col xs={24} sm={24} md={24} lg={24} xl={24} >
+                                    <CoursesTable />
+                                </Col>
+                            </Row>
                         </Col>
-                        <Col xs={24} sm={24} md={12} lg={12} xl={8} className="">
-                            <Card bordered={false} className="criclebox h-full">
-                                <div className="timeline-box">
-                                    <Title level={2}>{t("learning-history")}</Title>
-                                    <Paragraph className="lastweek" style={{ marginBottom: 24 }}>
-                                        {t("this-month")} <span>20%</span>
-                                    </Paragraph>
-
-                                    <Timeline
-                                        className="timelinelist"
-                                        reverse={reverse}
-                                    >
-                                        {timelineList.map((t, index) => (
-                                            <Timeline.Item color={t.color} key={index}>
-                                                <Title level={5}>{t.title}</Title>
-                                                <p>{t.time}</p>
-                                            </Timeline.Item>
-                                        ))}
-                                    </Timeline>
-                                    <Button
-                                        type="primary"
-                                        className="reverse-button"
-                                        onClick={() => setReverse(!reverse)}
-                                    >
-                                        {<MenuFoldOutlined />} {t("reverse")}
-                                    </Button>
-                                </div>
-                            </Card>
+                        <Col xs={24} sm={24} md={12} lg={12} xl={8} >
+                            <LearningHistory />
                         </Col>
                     </Row>
                 </div>
