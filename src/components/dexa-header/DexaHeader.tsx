6c528beb71796c1eac7c9695bf18313aec206096
alwandexa <alwan.wirawan@djembatandua.com>
2024-02-15 13:52:58 +0700

    change language selector

--- src/components/dexa-header/DexaHeader.tsx
+++ src/components/dexa-header/DexaHeader.tsx
@@ -1,22 +1,18 @@
-import { Button, Select } from "antd";
+import { Button } from "antd";
 import { useNavigate } from "react-router-dom";
 import { useTranslation } from "react-i18next";
-
-import i18n from "../../internalization/i18n";
-import "../../css/header.css";
 import { LogoutOutlined } from "@ant-design/icons";
+
+import LanguageSelector from "../language-selector/LanguageSelector";
+import "../../css/header.css";
 
 const DexaHeader = () => {
     const { t } = useTranslation();
     const navigate = useNavigate();
-    
+
     const logOut = () => {
         localStorage.removeItem("isLoggedIn");
         navigate("/");
-    };
-
-    const changeLanguage = (language: string) => {
-        i18n.changeLanguage(language);
     };
 
     return (
@@ -24,13 +20,7 @@ const DexaHeader = () => {
             <img src={process.env.PUBLIC_URL + "logo-white.svg"} alt="Company Logo" />
             <input type="search" placeholder={t("search-placeholder")} />
             <div>
-                <Select
-                    defaultValue={i18n.language}
-                    onChange={(language) => changeLanguage(language)}
-                    options={[
-                        { value: 'en', label: 'EN' },
-                        { value: 'id', label: 'ID' },]}
-                />
+                <LanguageSelector />
                 <Button type="primary" onClick={logOut} danger><LogoutOutlined style={{ fontSize: "1.5em" }} /></Button>
             </div>
         </>
