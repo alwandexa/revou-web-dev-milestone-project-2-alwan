ae9a7befa38c613d1427dff1bbf5ffdcf5c32a54
alwandexa <alwan.wirawan@djembatandua.com>
2024-02-15 11:25:11 +0700

    refactor login component

--- src/pages/login/Login.tsx
+++ src/pages/login/Login.tsx
@@ -1,76 +1,15 @@
-import { useState } from "react";
-import { Alert, Button, Card, Form, Input, Layout } from "antd";
+import { Layout } from "antd";
 import { Content } from "antd/es/layout/layout";
-import { useTranslation } from "react-i18next";
-import { useNavigate } from "react-router-dom";
 
 import "../../css/login.css";
+import LoginCard from "../../components/login-card/LoginCard";
 
 const Login = () => {
-    const [form] = Form.useForm();
-    const { t } = useTranslation();
-    const navigate = useNavigate();
-    const [visible, setVisible] = useState(false);
-
-    const checkCredential = () => {
-        const bcrypt = require('bcryptjs');
-        const users = JSON.parse(localStorage.getItem("users") as string);
-        let check = false;
-
-        users.forEach((user: any, index: number) => {
-            if(user.username) {
-                const checkUsername = form.getFieldValue("username") === user.username;
-                const checkHash = bcrypt.compareSync(form.getFieldValue("password"), user.hash);
-
-                if (checkUsername && checkHash) {
-                    check = true;
-                    localStorage.setItem("isLoggedIn", "true");
-                    localStorage.setItem("userIndex", index.toString());
-                    navigate("/");
-                }
-            }
-        });
-
-        if (!check) {
-            setVisible(true);
-            setTimeout(() => {
-                setVisible(false);
-            }, 2000);
-        }
-    }
-
     return (
         <>
             <Layout>
                 <Content className="login-content">
-                    <Card className="login-card">
-                        <img src={process.env.PUBLIC_URL + "logo-white.png"} alt="Company Logo" />
-                        <h1>{t("sign-in")}</h1>
-                        <p>{t("new-to-dexademy")} <a href="/registration">{t("create-account")}</a></p>
-                        {visible && <Alert className="login-alert" message="Check your username or password" type="error" showIcon />}
-                        <Form form={form} layout="vertical" onFinish={checkCredential} hideRequiredMark>
-                            <Form.Item
-                                name="username"
-                                label={t("username")}
-                                rules={[{ type: "string", required: true }]}
-                            >
-                                <Input />
-                            </Form.Item>
-
-                            <Form.Item
-                                name="password"
-                                label={t("password")}
-                                rules={[{ type: "string", required: true }]}
-                            >
-                                <Input.Password />
-                            </Form.Item>
-                            <p>
-                                <input type="checkbox" name="" id="" />{t("keep-me-signed-in")}
-                            </p>
-                            <Button htmlType="submit" className="login-button">{t("sign-in")}</Button>
-                        </Form>
-                        <p>{t("need-to-find")} <a href="/">{t("your-password")}</a></p>
-                    </Card>
+                    <LoginCard />
                 </Content>
             </Layout>
         </>
