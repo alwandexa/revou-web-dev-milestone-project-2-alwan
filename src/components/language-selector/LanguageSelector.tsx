6cd19b277730b4547d8f4eaac6e5e45527ad9158
alwandexa <alwan.wirawan@djembatandua.com>
2024-02-15 13:50:08 +0700

    create language selector component

---
+++ src/components/language-selector/LanguageSelector.tsx
@@ -0,0 +1,21 @@
+import { Select } from "antd";
+
+import i18n from "../../internalization/i18n";
+
+const LanguageSelector = () => {
+    const changeLanguage = (language: string) => {
+        i18n.changeLanguage(language);
+    };
+
+    return (
+        <Select
+            defaultValue={i18n.language}
+            onChange={(language) => changeLanguage(language)}
+            options={[
+                { value: 'en', label: 'EN' },
+                { value: 'id', label: 'ID' },]}
+        />
+    )
+}
+
+export default LanguageSelector;
